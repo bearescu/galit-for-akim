92b3927c1bdf61ef2054dfe9c20f8b0836278ad0
Beatriz Arespacochaga <bearescu@gmail.com>
2020-05-04 14:59:55 +0200

    index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get '/products', to: 'products#index'
+  # get '/products/:id', to: 'products#show'
+  # get '/products/new', to: 'products#new'
+  # post '/products', to: 'products#create'
+  # get '/products/:id/edit', to: 'products#edit'
+  # patch '/products/:id/edit', to: 'products#update'
+  # delete '/products/:id', to: 'products#destroy'
+  resources :products
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_02_132205) do
+ActiveRecord::Schema.define(version: 2020_05_02_154149) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,5 +21,7 @@ ActiveRecord::Schema.define(version: 2020_05_02_132205) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
   end
+
 end
